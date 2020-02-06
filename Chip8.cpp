163a5d9fa894fe6975991a26283e16fa403c8069
Petros <pokemongiant@yahoo.com>
2020-02-06 18:38:37 +0200

    Files inititialized
    main.cpp, Chip8.cpp and Chip8.h initialized.

---
+++ Chip8.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petros on 2/6/2020.
+//
+
+#include "Chip8.h"
---
+++ Chip8.h
@@ -0,0 +1,8 @@
+//
+// Created by Petros on 2/6/2020.
+//
+
+#ifndef CHIP8VM_CHIP8_H
+#define CHIP8VM_CHIP8_H
+
+#endif //CHIP8VM_CHIP8_H
