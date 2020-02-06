74378e7678dd959db05e3b0e9f8ef9a40cf9d9b5
Petros <pokemongiant@yahoo.com>
2020-02-06 20:19:54 +0200

    Class header file created

--- Chip8.h
+++ Chip8.h
@@ -4,5 +4,31 @@
 
 #ifndef CHIP8VM_CHIP8_H
 #define CHIP8VM_CHIP8_H
+class Chip8 {
+    private:
+        unsigned short opcode;			//current opcode all two bytes long
+        unsigned char memory[4096];		//Chip8 memory has 4K memory
+        unsigned char V[16];			//There are 16 register
 
+        unsigned short I;				//
+        unsigned short pc;				// pc counter
+        unsigned char gfx[64 * 32];		//Array for graphics (holds the pixel state 1 or 0)
+
+        unsigned short stack[16];		//Chip8 uses a stack
+        unsigned short sp;				//stack pointer
+
+        unsigned char key[16];			//HEX based keypad (0x0-0xF)
+
+        unsigned char delayTimer;		//The two timer registers that count at 60 Hz
+        unsigned char soundTimer;		//The system’s buzzer sounds whenever the sound timer reaches zero
+
+    public:
+        void initialize();
+        void emulateCycle();
+        void loadGame(const char* romPath);
+        bool drawFlag();
+        void setKeys();
+
+
+};
 #endif //CHIP8VM_CHIP8_H
